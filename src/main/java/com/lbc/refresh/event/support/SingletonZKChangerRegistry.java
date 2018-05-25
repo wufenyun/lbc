ac5ffaa83c46fcf818229396fd7bf62ade7b067b
wufenyun <wufenyun@zbj.com>
2018-05-25 17:50:07 +0800

    新增ZK监控模式下辅助类

---
+++ src/main/java/com/lbc/refresh/event/support/SingletonZKChangerRegistry.java
@@ -0,0 +1,30 @@
+package com.lbc.refresh.event.support;
+
+import com.lbc.support.AssertUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @description: 创建并注册单例ZkCacheStatusChager
+ * @author: wufenyun
+ * @date: 2018-05-25 16
+ **/
+public final class SingletonZKChangerRegistry {
+
+    private final Map<String,ZkCacheStatusChanger> changerMap = new ConcurrentHashMap<>();
+
+    public ZkCacheStatusChanger createOrGetChanger(String zkConnection,String refresherDataNodeName) {
+        AssertUtil.notBlank(zkConnection,"zookeeper地址不能为空");
+
+        String key = refresherDataNodeName + zkConnection;
+        if(null != changerMap.get(key)) {
+            return changerMap.get(key);
+        } else {
+            ZkCacheStatusChanger changer = new ZkCacheStatusChanger(zkConnection, "rms");
+            changerMap.put(key,changer);
+            return changer;
+        }
+    }
+}
